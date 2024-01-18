30c93a31ee4fd99f4b230cb844eeb304d0452e7f
Freyja Brandel-Tanis <30274062+freyja-bt@users.noreply.github.com>
2024-01-18 16:20:52 -0500

    styling updates; documentation; new function "cr_check" to circumvent issues with nesting empty tibbles when there are no documents available.

--- R/geopi_extract_sf.R
+++ R/geopi_extract_sf.R
@@ -1,40 +1,45 @@
-library(tidyverse)
-library(sf)
-library(httr2)
-library(glue)
-library(polite)
-library(rvest)
+#' Get GeoPI Spatial Data
+#'
+#' Get the spatial geometry of a GDOT project. `get_geopi_sf2` uses the httr2 package.
+#'
+#' @param gdot_pi GDOT Project ID
+#'
+#' @return An sf tibble
+#' @export
+#'
+#' @examples
+get_geopi_sf <- function(gdot_pi) {
+  req_url <- glue::glue("https://rnhp.dot.ga.gov/hosting/rest/services/GEOPI_APP/MapServer/0/query?f=json&where=PROJECT_ID%3D%27{gpi}%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects", gpi = gdot_pi)
 
-
-geopi_sf2 <- function(gdot_pi){
-  req_url <- request("https://rnhp.dot.ga.gov/")
-
-  resp <- req_url%>%
-    req_url_path("hosting/rest/services/GEOPI_APP/MapServer/0/query")%>%
-    req_url_query(
-    `f`="json",
-      `where`=glue("PROJECT_ID=\'{gpi}\'",gpi=gdot_pi),
-      `returnGeometry`="true",
-    `spatialRel`="esriSpatialRelIntersects"
-    )%>%
-    req_perform()
-
-  gpi_sf <- read_sf(resp$url)
-
-  return(gpi_sf)
-}
-
-
-get_geopi_sf <-  function(gdot_pi){
-
-  req_url <- glue("https://rnhp.dot.ga.gov/hosting/rest/services/GEOPI_APP/MapServer/0/query?f=json&where=PROJECT_ID%3D%27{gpi}%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects",gpi=gdot_pi)
-
-  gpi_sf <- read_sf(req_url)%>%
-    mutate(
+  gpi_sf <- sf::read_sf(req_url) %>%
+    dplyr::mutate(
       Project.ID = gdot_pi
-    )%>%
-    group_by(Project.ID)%>%
-    summarise()
+    ) %>%
+    dplyr::group_by(Project.ID) %>%
+    dplyr::summarise()
+
+  return(gpi_sf)
+}
+
+get_geopi_sf2 <- function(gdot_pi) {
+  req_url <- httr2::request("https://rnhp.dot.ga.gov/")
+
+  resp <- req_url %>%
+    httr2::req_url_path("hosting/rest/services/GEOPI_APP/MapServer/0/query") %>%
+    httr2::req_url_query(
+      `f` = "json",
+      `where` = glue::glue("PROJECT_ID=\'{gpi}\'", gpi = gdot_pi),
+      `returnGeometry` = "true",
+      `spatialRel` = "esriSpatialRelIntersects"
+    ) %>%
+    httr2::req_perform()
+
+  gpi_sf <- sf::read_sf(resp$url) %>%
+    dplyr::mutate(
+      Project.ID = gdot_pi
+    ) %>%
+    dplyr::group_by(Project.ID) %>%
+    dplyr::summarise()
 
   return(gpi_sf)
 }
@@ -42,84 +47,172 @@ get_geopi_sf <-  function(gdot_pi){
 
 
 
-get_geopi <- function(gdot_pi, geometry = FALSE){
+#' Get GeoPI Data
+#'
+#' Get the available GeoPI data for a project. Adds the field `Gather.Date` with the current date to document the date of extraction.
+#'
+#' @param gdot_pi GDOT Project ID. Can be a single PI or a vector or list of PIs. If more than one PI is provided, each project will get its own row.
+#' @param geometry if FALSE (the default), return a regular tibble of GeoPI data. if TRUE, uses the get_geopi_sf function to return an sf tibble with simple feature geometry in the 'geometry' column.
+#'
+#' @return A tibble or an sf tibble depending on if geometry is FALSE or TRUE
+#' @export
+#'
+#' @examples
+get_geopi <- function(gdot_pi, geometry = FALSE) {
+  session <- polite::bow("https://www.dot.ga.gov/applications/geopi/Pages/Dashboard.aspx")
 
-  session <- bow("https://www.dot.ga.gov/applications/geopi/Pages/Dashboard.aspx")
-
-  geopi_data <- map_dfr(
+  geopi_data <- purrr::map_dfr(
     .x = gdot_pi,
-    .f = function(gdot_pi=.x){
+    .f = function(gdot_pi = .x) {
+      gather_date <- lubridate::today()
 
-  gather_date = today()
+      # table of project details (location, etc). column 1 and 3 are headings, 2 and 4 are details.
+      # row 1 of all columns is the project name
+      project_overview <- polite::scrape(session, query = list(ProjectId = gdot_pi)) %>%
+        rvest::html_node("#ctl00_ctl51_g_f10b03f0_acac_4f79_94ee_72b988c6533b_ctl09 > table") %>%
+        rvest::html_table()
 
-  # table of project details (location, etc). column 1 and 3 are headings, 2 and 4 are details.
-  # row 1 of all columns is the project name
-  project_overview <- scrape(session, query = list(ProjectId=gdot_pi))%>%html_node("#ctl00_ctl51_g_f10b03f0_acac_4f79_94ee_72b988c6533b_ctl09 > table")%>%html_table()
+      # table of project description
+      project_description <- polite::scrape(session, query = list(ProjectId = gdot_pi)) %>%
+        rvest::html_node("#ctl00_ctl51_g_fa9abf0f_0f34_4a8a_8910_0a437a68f590_ctl09 > table") %>%
+        rvest::html_table()
+      # Table of activity / program year / cost estimate / date of estimate
+      phase_details <- polite::scrape(session, query = list(ProjectId = gdot_pi)) %>%
+        rvest::html_node("#ctl00_ctl51_g_b8d3a566_e67d_4ccb_95d8_24b07c277a46_g_b8d3a566_e67d_4ccb_95d8_24b07c277a46DataGrid_ctl00") %>%
+        rvest::html_table()
 
-  # table of project description
-  project_description <- scrape(session, query = list(ProjectId=gdot_pi))%>%html_node("#ctl00_ctl51_g_fa9abf0f_0f34_4a8a_8910_0a437a68f590_ctl09 > table")%>%html_table()
-  # Table of activity / program year / cost estimate / date of estimate
-  phase_details <- scrape(session, query = list(ProjectId=gdot_pi))%>%html_node("#ctl00_ctl51_g_b8d3a566_e67d_4ccb_95d8_24b07c277a46_g_b8d3a566_e67d_4ccb_95d8_24b07c277a46DataGrid_ctl00")%>%html_table()
+      proj_desc_tidy <- dplyr::bind_rows(
+        tibble::tibble(name = "Project Name", value = project_overview[[1, 1]]),
+        project_overview[2:nrow(project_overview), c("X1", "X2")] %>% dplyr::rename("name" = "X1", "value" = "X2"),
+        project_overview[2:7, c("X3", "X4")] %>% dplyr::rename("name" = "X3", "value" = "X4"), # nrow(project_overview)
+        tibble::tibble(name = "Project Description", value = project_description[[2, 1]])
+      ) %>%
+        dplyr::mutate(
+          name = stringr::str_replace(name, ":", ""),
+          value = dplyr::na_if(value, "formatDate(\'\')"),
+          value = dplyr::na_if(value, "formatCurrency(\'0.0000\')"),
+          value = dplyr::na_if(value, "%"),
+          value = dplyr::na_if(value, "")
+        )
 
-  proj_desc_tidy <- bind_rows(
-    tibble(name = "Project Name", value = project_overview[[1,1]]),
-    project_overview[2:nrow(project_overview),c("X1","X2")]%>%rename("name"="X1","value"="X2"),
-    project_overview[2:7,c("X3","X4")]%>%rename("name"="X3","value"="X4"), # nrow(project_overview)
-    tibble(name = "Project Description", value = project_description[[2,1]])
-  )%>%
-    mutate(
-      name = str_replace(name, ":", ""),
-      value = na_if(value, "formatDate(\'\')"),
-      value = na_if(value, "formatCurrency(\'0.0000\')"),
-      value = na_if(value, "%"),
-      value = na_if(value, "")
-    )
+      proj_desc_wide <- proj_desc_tidy %>%
+        tidyr::pivot_wider() %>%
+        tibble::as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet = T)) %>%
+        dplyr::mutate(
+          dplyr::across(
+            tidyselect::ends_with(".Date"),
+            .fns = lubridate::as_date
+          ),
+          Gather.Date = gather_date
+        )
 
-  proj_desc_wide <- proj_desc_tidy%>%
-    pivot_wider()%>%
-    as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet=T))%>%
-    mutate(
-      across(
-        ends_with(".Date"),
-        .fns = as_date
-      ),
-      Gather.Date = gather_date
-    )
+      phase_details_wide <- phase_details %>%
+        tibble::as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet = T)) %>%
+        dplyr::mutate(
+          Date.of.Last.Estimate = lubridate::mdy(Date.of.Last.Estimate),
+          Cost.Est.USD = stringr::str_replace_all(Cost.Estimate, "[\\$,]", "") %>% as.numeric(),
+          phase_abbr = stringr::str_extract(Activity, "[:alpha:]+")
+        ) %>%
+        dplyr::select(-Activity, -Cost.Estimate) %>%
+        tidyr::pivot_wider(
+          names_from = phase_abbr,
+          values_from = c(Program.Year, Date.of.Last.Estimate, Cost.Est.USD),
+          names_glue = "{phase_abbr}_{.value}",
+          names_vary = "slowest"
+        )
 
-  phase_details_wide <- phase_details%>%
-    as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet=T))%>%
-    mutate(
-      Date.of.Last.Estimate = mdy(Date.of.Last.Estimate),
-      Cost.Est.USD = str_replace_all(Cost.Estimate, "[\\$,]","")%>%as.numeric(),
-      phase_abbr = str_extract(Activity,"[:alpha:]+")
-    )%>%
-    select(-Activity, -Cost.Estimate)%>%
-    pivot_wider(
-      names_from = phase_abbr,
-      values_from = c(Program.Year, Date.of.Last.Estimate, Cost.Est.USD),
-      names_glue = "{phase_abbr}_{.value}",
-      names_vary = "slowest"
-    )
+      document_inclusion <- polite::scrape(session, query = list(ProjectId = gdot_pi)) %>%
+        rvest::html_node("#ctl00_ctl51_g_c9069b02_6998_4434_b04d_f108ef5b6961_g_c9069b02_6998_4434_b04d_f108ef5b6961DataGrid_ctl00") %>%
+        rvest::html_table() %>%
+        tibble::as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet = T)) %>%
+        dplyr::mutate(...1 = NULL, dplyr::across(.cols = tidyselect::everything(), .fns = \(x) dplyr::na_if(x, ""))) %>%
+        tidyr::fill(Project.Documents) %>%
+        dplyr::filter(Project.Documents != FILE_PATH) %>%
+        dplyr::mutate(Project.Documents = stringr::str_replace_all(Project.Documents, "\\s", "\\.")) %>%
+        dplyr::select(
+          DOC_TYPE = Project.Documents,
+          tidyselect::everything()
+        )
 
-  project_details_df <- bind_cols(proj_desc_wide, phase_details_wide)
+      concept_reports <- document_inclusion %>%
+        dplyr::filter(DOC_TYPE == "Approved.Concept.Reports") %>%
+        tidyr::separate_wider_regex(
+          FILE_NAME,
+          c(Project.ID = "[:digit:]{6,7}-?", "_", File.Type = "[:alpha:]*", "_", File.Date = "[:alnum:]+", "\\.pdf"),
+          cols_remove = F
+        )
 
-  if(geometry == TRUE){
+      project_details_df <- dplyr::bind_cols(proj_desc_wide, phase_details_wide) %>%
+        dplyr::mutate(
+          Documents.Exists = dplyr::if_else(nrow(document_inclusion) > 0, TRUE, FALSE),
+          # Documents = tidyr::nest(document_inclusion),
+          CR.Exists = dplyr::if_else(nrow(concept_reports) > 0, TRUE, FALSE),
 
-      req_url <- glue("https://rnhp.dot.ga.gov/hosting/rest/services/GEOPI_APP/MapServer/0/query?f=json&where=PROJECT_ID%3D%27{gpi}%27&returnGeometry=true&spatialRel=esriSpatialRelIntersects",gpi=gdot_pi)
+        )%>%
+        {if(nrow(concept_reports) > 0){
+          .%>%
+            mutate(
+              Concept.Reports = tidyr::nest(concept_reports)
+            )
+        }}%>%
+        {if(nrow(document_inclusion) > 0){
+          .%>%
+            mutate(
+              Documents = tidyr::nest(document_inclusion)
+            )
+        }}
 
-      gpi_sf <- get_geopi_sf(gdot_pi)
 
-    project_details_df <- full_join(project_details_df, gpi_sf, by = "Project.ID")%>%
-      st_as_sf()
+      if (geometry == TRUE) {
+        gpi_sf <- get_geopi_sf(gdot_pi)
 
-  }
+        project_details_df <- dplyr::full_join(project_details_df, gpi_sf, by = "Project.ID") %>%
+          sf::st_as_sf()
+      }
 
-  return(project_details_df)
-  }
-
-)
+      return(project_details_df)
+    }
+  )
   return(geopi_data)
-
 }
 
 
+cr_check <- function(gdot_pi){
+
+  session <- polite::bow("https://www.dot.ga.gov/applications/geopi/Pages/Dashboard.aspx")
+      gather_date <- lubridate::today()
+
+  geopi_data <- purrr::map_dfr(
+    .x = gdot_pi,
+    .f = function(gdot_pi = .x) {
+      message("Checking CR for ",gdot_pi)
+  document_inclusion <- polite::scrape(session, query = list(ProjectId = gdot_pi)) %>%
+    rvest::html_node("#ctl00_ctl51_g_c9069b02_6998_4434_b04d_f108ef5b6961_g_c9069b02_6998_4434_b04d_f108ef5b6961DataGrid_ctl00") %>%
+    rvest::html_table() %>%
+    tibble::as_tibble(.name_repair = ~ vctrs::vec_as_names(..., repair = "universal", quiet = T)) %>%
+    dplyr::mutate(...1 = NULL, dplyr::across(.cols = tidyselect::everything(), .fns = \(x) dplyr::na_if(x, ""))) %>%
+    tidyr::fill(Project.Documents) %>%
+    dplyr::filter(Project.Documents != FILE_PATH) %>%
+    dplyr::mutate(Project.Documents = stringr::str_replace_all(Project.Documents, "\\s", "\\.")) %>%
+    dplyr::select(
+      DOC_TYPE = Project.Documents,
+      tidyselect::everything()
+    )
+
+  concept_reports <- document_inclusion %>%
+    dplyr::filter(DOC_TYPE == "Approved.Concept.Reports") %>%
+    tidyr::separate_wider_regex(
+      FILE_NAME,
+      c(Project.ID = "[:digit:]{6,7}-?", "_", File.Type = "[:alpha:]*", "_", File.Date = "[:alnum:]+", "\\.pdf"),
+      cols_remove = F
+    )
+
+  cr_tbl <- tibble::tibble(
+    Project.ID = gdot_pi,
+    CR.Exists = dplyr::if_else(nrow(concept_reports) > 0, TRUE, FALSE)
+  )
+  return(cr_tbl)
+    }
+  )
+  return(geopi_data)
+}
