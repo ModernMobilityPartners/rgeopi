519873c08b2bfcf1999c59e829fa02804acbb9ee
Freyja Brandel-Tanis <30274062+freyja-bt@users.noreply.github.com>
2025-05-30 11:24:34 -0400

    feat: fill in place road name and bounds for inter* projects

--- R/place.R
+++ R/place.R
@@ -4,7 +4,35 @@
 #'
 #' @returns sf object
 #' @export
-format_place_gis <- function(project_sf) {
+format_place_gis <- function(project_sf, project_overview = NULL) {
+  if (!is.null(project_overview)) {
+    project_overview <- project_overview %>%
+      dplyr::mutate(
+        road_name = dplyr::case_when(
+          stringr::str_detect(Project.Name, "@") ~ stringr::str_squish(stringr::str_split_1(Project.Name, "@")[1]),
+          .default = Project.Name
+        ),
+        proj_begin = dplyr::case_when(
+          stringr::str_detect(Project.Name, "@") ~ stringr::str_squish(stringr::str_split_1(Project.Name, "@")[1]),
+          .default = NA_character_
+        ),
+        proj_end = dplyr::case_when(
+          stringr::str_detect(Project.Name, "@") ~ stringr::str_squish(stringr::str_split_1(Project.Name, "@")[2]),
+          .default = NA_character_
+        )
+      ) %>%
+      dplyr::select(
+        project_id = Project.ID, road_name, proj_begin, proj_end
+      )
+  } else {
+    project_overview <- data.frame(
+      "project_id" = project_sf$Project.ID,
+      "road_name" = rep(NA_character_, nrow(project_sf)), # required: project road/route Name:
+      "proj_begin" = rep(NA_character_, nrow(project_sf)), # optional: Project Begin Terminus/Cross Street 1
+      "proj_end" = rep(NA_character_, nrow(project_sf)) # optional: Project End Terminus/Cross Street 2
+    )
+  }
+
   project_sf <- project_sf %>%
     dplyr::rename(
       project_id = Project.ID
@@ -14,14 +42,22 @@ format_place_gis <- function(project_sf) {
         "proj_elem" = NA_integer_, # required: project element id 0, 1, 2, 3...
         "proj_type" = NA_character_, # required: project element type
         "proj_sub" = NA_character_, # required: project element sub-type
-        "road_name" = NA_character_, # required: project road/route Name:
-        "proj_begin" = NA_character_, # optional: Project Begin Terminus/Cross Street 1
-        "proj_end" = NA_character_, # optional: Project End Terminus/Cross Street 2
         "PE_year" = NA_integer_, # optional: Year, PE is likely to begin
         "est_name" = NA_character_, # optional: Estimators Name
         "est_org" = NA_character_, # optional: Estimators Organization
         "study_name" = NA_character_ # optional: Associated Study
       )
+    ) %>%
+    dplyr::full_join(
+      project_overview, by = dplyr::join_by(project_id)
+    ) %>%
+    dplyr::select(
+      project_id, proj_elem, proj_type, proj_sub, road_name, proj_begin, proj_end, PE_year, est_name, est_org, study_name
+    ) %>%
+    dplyr::mutate(
+      proj_elem = dplyr::row_number(), .by = project_id
     )
+
+
   return(project_sf)
 }
